7f36c9b999ce41c87d0d0ba96dec8940a6c913b8
TianTianny <acdeocera.bb88@gmail.com>
2024-08-12 17:20:20 +0800

    models additions

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
+
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -18,8 +25,17 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'username',
         'email',
+        'phone',
+        'birthday',
+        'gender',
         'password',
+        'cover_path',
+        'avatar_path',
+        'role',
+        'is_admin'
     ];
 
     /**
@@ -43,5 +59,20 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Determine if the user can access the specified Filament panel.
+     *
+     * @param  \Filament\Panel  $panel
+     * @return bool
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'admin') {
+            return $this->is_filament_admin;
+        }
+
+        return true;
     }
 }
