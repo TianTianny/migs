b8459544e7e33a027e0ed1e8ce49ce6b68532fe6
TianTianny <acdeocera.bb88@gmail.com>
2024-08-12 17:23:31 +0800

    add middleware for filament admin panel

---
+++ app/Http/Middleware/EnsureIsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
+
+        // Redirect or abort with 403 Forbidden if not admin
+        abort(403, 'Unauthorized');
+    }
+}
