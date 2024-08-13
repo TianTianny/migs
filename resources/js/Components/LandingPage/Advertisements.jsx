c22faefdec7cb3e1151815a5368ca78a80ca432b
TianTianny <acdeocera.bb88@gmail.com>
2024-08-13 16:35:20 +0800

    reusable ads

--- resources/js/Components/LandingPage/Advertisements.jsx
+++ resources/js/Components/LandingPage/Advertisements.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Advertisements = () => {
+const Advertisements = ({ ads }) => {
     return (
-        <>
-            <div>
-                <h1>Advertisements</h1>
-            </div>
-        </>
+        <div className="flex flex-col sm:flex-row gap-2 justify-center px-4">
+            {ads.map((ad, index) => (
+                <div key={index} className="w-full sm:w-1/2 lg:w-1/2 p-4">
+                    <img
+                        src={ad.src}
+                        alt={ad.alt}
+                        className="w-full h-auto shadow-lg rounded-md"
+                    />
+                </div>
+            ))}
+        </div>
     );
+};
+
+Advertisements.propTypes = {
+    ads: PropTypes.arrayOf(
+        PropTypes.shape({
+            src: PropTypes.string.isRequired,
+            alt: PropTypes.string.isRequired,
+        })
+    ).isRequired,
 };
 
 export default Advertisements;
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -11,6 +11,24 @@ import {
 } from "@/Sections";
 import { Navbar, Footer, Advertisements } from "@/Components/LandingPage";
 
+import ads from "/public/image/Advertisements/ADS 1.jpg";
+import adsT from "/public/image/Advertisements/ADS 2.jpg";
+
+const ads1 = [
+    { src: ads, alt: "Ad 1" },
+    { src: adsT, alt: "Ad 2" },
+];
+
+const ads2 = [
+    { src: ads, alt: "Ad 3" },
+    { src: adsT, alt: "Ad 4" },
+];
+
+const ads3 = [
+    { src: ads, alt: "Ad 5" },
+    { src: adsT, alt: "Ad 6" },
+];
+
 export default function Welcome({ heroes }) {
     return (
         <>
@@ -18,14 +36,20 @@ export default function Welcome({ heroes }) {
             <Navbar />
             <section className="max-w-7xl mx-auto">
                 <Hero heroes={heroes} />
-                <Advertisements />
+                <section className="py-10 bg-primary rounded-md">
+                    <Advertisements ads={ads1} />
+                </section>
                 <AboutUs />
                 <MissionVisionValues />
                 <Programs />
                 <AdditionalPrograms />
-                <Advertisements />
+                <section className="py-10 bg-gray-300">
+                    <Advertisements ads={ads2} />
+                </section>
                 <WhyChooseUs />
-                <Advertisements />
+                <section className="py-5 bg-bgSkyBlue">
+                    <Advertisements ads={ads3} />
+                </section>
                 <Testimonials />
                 <ContactUs />
                 <Footer />
