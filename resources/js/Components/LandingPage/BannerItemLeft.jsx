19e376cc7db38c4c906488df09c3072e90283b18
TianTianny <acdeocera.bb88@gmail.com>
2024-08-21 11:25:32 +0800

    banners

--- resources/js/Components/LandingPage/BannerItemLeft.jsx
+++ resources/js/Components/LandingPage/BannerItemLeft.jsx
@@ -7,14 +7,16 @@ const BannerItemLeft = ({ image, number, title, description }) => {
             style={{ backgroundImage: `url(${image})`, height: "140px" }}
         >
             {/* Title */}
-            <div className="absolute flex flex-col items-center justify-center text-center text-white transition-transform duration-500 group-hover:-translate-y-8 group-hover:scale-75">
-                <h2 className="text-lg md:text-xl font-semibold">{title}</h2>
+            <div className="absolute flex flex-col px-32 items-center md:me-16 justify-center text-center text-white transition-transform duration-500 group-hover:-translate-y-8 group-hover:scale-75 group-hover:px-16">
+                <h2 className="text-lg uppercase md:text-3xl group-hover:text-xl font-semibold transition-all duration-400 ease-in-out">
+                    {title}
+                </h2>
             </div>
 
             {/* Description */}
             {description && (
                 <div className="absolute bottom-4 md:bottom-6 left-1/2 transform -translate-x-1/2 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
-                    <p className="text-xs md:text-xs text-white">
+                    <p className="text-xs md:text-xs tracking-wide text-white me-10">
                         {description}
                     </p>
                 </div>
